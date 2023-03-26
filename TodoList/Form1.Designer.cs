82811c29280a323d817c0b31d49db3105f17b021
Vivekgunjal <vivekgunjal619@gmail.com>
2023-03-26 12:04:27 +0530

    Add project files.

---
+++ TodoList/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TodoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDeleteTodo = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtEdit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkIsCompleted = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(189, 56);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input:";
+            // 
+            // btnDeleteTodo
+            // 
+            this.btnDeleteTodo.Location = new System.Drawing.Point(478, 420);
+            this.btnDeleteTodo.Name = "btnDeleteTodo";
+            this.btnDeleteTodo.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteTodo.TabIndex = 5;
+            this.btnDeleteTodo.Text = "Delete";
+            this.btnDeleteTodo.UseVisualStyleBackColor = true;
+            this.btnDeleteTodo.Click += new System.EventHandler(this.btnDeleteTodo_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(397, 420);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(111, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 313);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit_1);
+            // 
+            // txtEdit
+            // 
+            this.txtEdit.Location = new System.Drawing.Point(189, 418);
+            this.txtEdit.Name = "txtEdit";
+            this.txtEdit.Size = new System.Drawing.Size(100, 20);
+            this.txtEdit.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter Text:";
+            // 
+            // chkIsCompleted
+            // 
+            this.chkIsCompleted.AutoSize = true;
+            this.chkIsCompleted.Location = new System.Drawing.Point(295, 421);
+            this.chkIsCompleted.Name = "chkIsCompleted";
+            this.chkIsCompleted.Size = new System.Drawing.Size(52, 17);
+            this.chkIsCompleted.TabIndex = 10;
+            this.chkIsCompleted.Text = "Done";
+            this.chkIsCompleted.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkIsCompleted);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEdit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnDeleteTodo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDeleteTodo;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtEdit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkIsCompleted;
+    }
+}
+
---
+++ TodoList/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using static System.Resources.ResXFileRef;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+
+namespace TodoList
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
+        private SQLiteDataAdapter adapter;
+        private readonly string connectionString = @"Data Source = todo.db; Version=3;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateTodoTable();
+            LoadTodos();
+
+        }
+
+        private void CreateTodoTable()
+        {
+            connection = new SQLiteConnection(connectionString);
+            string query = "CREATE TABLE IF NOT EXISTS Todos (Id INTEGER PRIMARY KEY AUTOINCREMENT, Description TEXT NOT NULL, IsCompleted INTEGER NOT NULL);";
+            command = new SQLiteCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+        private void LoadTodos()
+        {
+            connection = new SQLiteConnection(connectionString);
+            string query = "SELECT * FROM Todos;";
+            adapter = new SQLiteDataAdapter(query, connection);
+
+            try
+            {
+                connection.Open();
+                var dataTable = new System.Data.DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection = new SQLiteConnection(connectionString);
+            string query = "INSERT INTO Todos (Description, IsCompleted) VALUES (@Description, @IsCompleted);";
+            command = new SQLiteCommand(query, connection);
+            command.Parameters.AddWithValue("@Description", txtDescription.Text);
+            command.Parameters.AddWithValue("@IsCompleted", 0);
+
+            try
+            {
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Todo added successfully.");
+                    LoadTodos();
+                    txtDescription.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Todo was not added.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int rowIndex = dataGridView1.SelectedRows[0].Index;
+                int todoId = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells[0].Value);
+
+                connection = new SQLiteConnection(connectionString);
+                string query = "UPDATE Todos SET Description=@Description, IsCompleted=@IsCompleted WHERE Id=@Id;";
+                command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@Description", txtEdit.Text);
+                command.Parameters.AddWithValue("@IsCompleted", chkIsCompleted.Checked ? 1 : 0);
+                command.Parameters.AddWithValue("@Id", todoId);
+                
+                try
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Todo updated successfully.");
+                        LoadTodos();
+                        txtEdit.Text = "";
+                        chkIsCompleted.Checked = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Todo was not updated.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+
+
+        private void btnDeleteTodo_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                connection = new SQLiteConnection(connectionString);
+                string query = "DELETE FROM Todos WHERE Id=@Id;";
+                command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value));
+
+                try
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Todo deleted successfully.");
+                        LoadTodos();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Todo was not deleted.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a todo to delete.");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtEdit.Text = row.Cells["Description"].Value.ToString();
+                chkIsCompleted.Checked = Convert.ToBoolean(row.Cells["IsCompleted"].Value);
+            }
+        }
+
+        private void dataGridView1_CellEndEdit_1(object sender, DataGridViewCellEventArgs e)
+        {
+            connection = new SQLiteConnection(connectionString);
+            string query = "UPDATE Todos SET Description=@Description, IsCompleted=@IsCompleted WHERE Id=@Id;";
+            command = new SQLiteCommand(query, connection);
+            command.Parameters.AddWithValue("@Description", dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+            command.Parameters.AddWithValue("@IsCompleted", Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value));
+            command.Parameters.AddWithValue("@Id", Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value));
+            txtEdit.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            try
+            {
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Todo updated successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Todo was not updated.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+    }
+}
